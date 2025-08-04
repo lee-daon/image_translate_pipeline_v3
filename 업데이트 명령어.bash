36a22d423127eca5eb0893ade627bba7d9999d3c
이다온 <leedaon480@gmail.com>
2025-08-04 13:19:32 +0900

    v0.1.0업데이트 완료

---
+++ 업데이트 명령어.bash
@@ -0,0 +1,12 @@
+# 1. image_translate_worker 디렉토리로 이동
+cd image_translate_worker
+
+# 2. 도커 이미지 빌드
+docker build -t leedaon/image_translate_worker:v0.1.0 .
+
+# 3. latest 태그도 추가 (선택사항)
+docker tag leedaon/image_translate_worker:v0.1.0 leedaon/image_translate_worker:latest
+
+# 4. Docker Hub에 푸시 (로그인 필요)
+docker push leedaon/image_translate_worker:v0.1.0
+docker push leedaon/image_translate_worker:latest
